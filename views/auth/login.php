92498ce40b67f7a7867ed0d34413ecf0b823e71a
Yasz Avellia <zakamaragames@gmail.com>
2022-09-08 20:35:10 +0700

    login tahap 2

--- views/auth/login.php
+++ views/auth/login.php
@@ -7,16 +7,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7">
-                <div class="card">
+    <div class="container position-relative parent">
+        <div class="row justify-content-center ">
+            <div class="col-lg-6">
+                <div class="card shadow-lg">
                     <div class="card-body">
                         <form action="" method="post">
-                            <label for="">Username</label>
-                            <input type="text" name="" placeholder="" id="" class="form-control">
-                            <label for="">Password</label>
-                            <input type="text" name="" placeholder="" id="" class="form-control">
+                            <div class="header mb-4 text-center">
+                                <h4>Sign In To CRUD</h4>
+                            </div>
+                            <label for="user">Username</label>
+                            <input type="text" name="username" placeholder="Masukkan Username" id="user" class="form-control mb-3">
+
+                            <label for="pw">Password</label>
+                            <input type="password" name="pw" placeholder="Masukkan Password" id="pw" class="form-control">
+
+                            <div class="footer mt-3">
+                                <button class="btn btn-primary">Masuk</button>
+                                <button class="btn btn-danger" type="reset">Reset</button>
+                            </div>
                         </form>
                     </div>
                 </div>
