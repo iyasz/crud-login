59d8972c8d55efc2fdee8be869f5caaf049d9f5d
Yasz Avellia <zakamaragames@gmail.com>
2022-09-15 19:58:07 +0700

    menambah input

--- views/admin/input.php
+++ views/admin/input.php
@@ -4,6 +4,33 @@ $conn = mysqli_connect('localhost', 'root', '', 'crudlog');
 
 $select = $conn->query("SELECT * FROM anggota");
 
+if(isset($_POST['submit'])){
+    $nama = htmlspecialchars($_POST['nama']);
+    $usename = htmlspecialchars($_POST['username']);
+    $telepon = htmlspecialchars($_POST['telepon']);
+    $alamat = htmlspecialchars($_POST['alamat']);
+
+    $simpan = $conn->query("INSERT INTO anggota VALUES  (NULL, '$nama', '$usename', '$telepon', '$alamat')");
+    if($simpan){
+        echo "<script>Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+          })</script>";
+          header('location: index.php');
+    } else {
+        echo "<script>Swal.fire({
+            position: 'top-end',
+            icon: 'error',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+          })</script>";
+    }
+}
+
 ?>
 
 <!doctype html>
@@ -12,8 +39,9 @@ $select = $conn->query("SELECT * FROM anggota");
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <title>Bootstrap demo</title>
 </head>
 
 <body>
@@ -28,22 +56,22 @@ $select = $conn->query("SELECT * FROM anggota");
                             </div>
                             <div class="mb-3">
                                 <label for="nama">Nama</label>
-                                <input type="text" name="nama" placeholder="" class="form-control" id="nama">
+                                <input type="text" required name="nama" placeholder="" class="form-control" id="nama">
                             </div>
                             <div class="mb-3">
                                 <label for="username">Username</label>
-                                <input type="text" name="username" placeholder="" class="form-control" id="username">
+                                <input type="text" name="username" required placeholder="" class="form-control" id="username">
                             </div>
                             <div class="mb-3">
                                 <label for="telp">No Telp</label>
-                                <input type="number" name="telpon" placeholder="" class="form-control" id="telp">
+                                <input type="number" name="telepon" required placeholder="" class="form-control" id="telp">
                             </div>
                             <div class="mb-3">
                                 <label for="alamat">Alamat</label>
-                                <input type="text" name="alamat" placeholder="" class="form-control" id="alamat">
+                                <input type="text" name="alamat" required placeholder="" class="form-control" id="alamat">
                             </div>
                             <div class="footer">
-                                <button class="btn btn-primary" type="submit">Submit</button>
+                                <button class="btn btn-primary" name="submit" type="submit">Submit</button>
                                 <button class="btn btn-danger" type="reset">Reset</button>
                             </div>
                         </form>
@@ -72,7 +100,7 @@ $select = $conn->query("SELECT * FROM anggota");
                                 <tr>
                                     <td> <?= $no++ ?> </td>
                                     <td> <?= $selects['nama'] ?> </td>
-                                    <td> <?= $selects['usernae'] ?> </td>
+                                    <td> <?= $selects['usernaMe'] ?> </td>
                                     <td> <?= $selects['telepon'] ?> </td>
                                     <td> <?= $selects['nama'] ?> </td>
                                     <td class="text-center">
